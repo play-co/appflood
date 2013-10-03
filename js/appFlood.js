683165caa6cd86bf69fbaf3f6908c220695976fd
Cory Dodt <corydodt@gmail.com>
2013-10-02 17:03:55 -0700

    showInterstitial on android

--- js/appFlood.js
+++ js/appFlood.js
@@ -1,12 +1,33 @@
-var AppFlood = Class(function () {
-	this.showInterstitial = function () {
+import event.Emitter;
+var AppFlood = Class(event.Emitter, function () {
+	this.showInterstitial = function (callback, cbArgs) {
 		logger.log("{appFlood} Showing interstitial");
 
 		if (NATIVE && NATIVE.plugins && NATIVE.plugins.sendEvent) {
 			NATIVE.plugins.sendEvent("AppFloodPlugin", "showInterstitial",
 				JSON.stringify({}));
 		}
+
+        this.callback = callback;
+        this.cbArgs = cbArgs;
 	};
+
+    this._initNATIVE = function _a_init() {
+        NATIVE.events.registerHandler('appflood', bind(this, function (evt) {
+            logger.log('{appFlood} ad is done');
+            this.emit('appflood:closed', this.callback, this.cbArgs);
+            return;
+        }));
+    };
+
+    this._initSimulated = function () {};
+
+    if (NATIVE && NATIVE.events && NATIVE.plugins) {
+        this.init = this._initNATIVE;
+    } else {
+        this.init = this._initSimulated;
+    }
+
 });
 
 exports = new AppFlood();
